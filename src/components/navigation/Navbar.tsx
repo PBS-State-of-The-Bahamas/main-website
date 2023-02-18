1e94fc8c8c575e1ede63a561514bfaffb69c03d4
Stefano Hepburn <stefanohepburn@gmail.com>
2023-02-17 19:26:32 -0500

    Added hover state to navbar

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -15,9 +15,9 @@ export default function Navbar({type = 'main'}: Props) {
         <div className='flex items-center ml-auto'>
           {
             renderNavigationLinks(mainNav[type])
-              .map((link, key) => <div key={key} className={`px-3 text-gray-1`}>{link}</div>)
+              .map((link, key) => <div key={key} className={`px-3 text-gray-1 hover:text-gray-3`}>{link}</div>)
           }
-          <button className='text-royal-blue bg-gray-1 px-8 py-2 rounded text-heading-6 uppercase'>Contact Us</button>
+          <button className='text-royal-blue bg-gray-1 px-8 py-2 rounded text-heading-6 uppercase hover:text-dark-royal-blue hover:bg-gray-2'>Contact Us</button>
         </div>
       </div>
     </>
@@ -29,7 +29,7 @@ function SubNavBar() {
     <div className='flex bg-dark-royal-blue text-gray-1 text-sm'>
       {
         renderNavigationLinks(subNav)
-          .map((link, key)=> <div key={key} className={`px-2 text-gray-1`}>{link}</div>)
+          .map((link, key)=> <div key={key} className={`px-2 text-gray-1 hover:text-gray-3`}>{link}</div>)
       }
     </div>
   )
