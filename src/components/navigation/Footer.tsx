12fff698eb15131d2052495ff717ff65b2ecf4c7
Stefano Hepburn <stefanohepburn@gmail.com>
2023-02-17 20:04:42 -0500

    Added padding to navbar and footer

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -6,23 +6,29 @@ type Props = {}
 function Footer({}: Props) {
   return (
     <>
-        <div className='flex space-x-20 bg-dark-royal-blue py-6'>
-            <div>
-                <h6 className='text-heading-6 uppercase text-gray-1'>Menu</h6>
-                <ul className='text-gray-1 mt-3'>
-                    {renderNavigationLinks(mainNav['main']).map((item, index)=> <li key={index}>{item}</li>)}
-                </ul>
+        <div className='bg-dark-royal-blue py-6'>
+            <div className='mx-auto flex space-x-20 max-w-screen-2xl px-10'>
+                <div>
+                    <h6 className='text-heading-6 uppercase text-gray-1'>Menu</h6>
+                    <ul className='text-gray-1 mt-3'>
+                        {renderNavigationLinks(mainNav['main']).map((item, index)=> <li key={index}>{item}</li>)}
+                    </ul>
+                </div>
+                <div>
+                    <h6 className='text-heading-6 uppercase text-gray-1'>Chapters</h6>
+                    <ul className='text-gray-1 mt-3 '>
+                        {renderNavigationLinks(subNav).map((item, index)=> <li className='hover:text-gray-3'key={index}>{item}</li>) }
+                    </ul>
+                </div>
             </div>
-            <div>
-                <h6 className='text-heading-6 uppercase text-gray-1'>Chapters</h6>
-                <ul className='text-gray-1 mt-3 '>
-                    {renderNavigationLinks(subNav).map((item, index)=> <li className='hover:text-gray-3'key={index}>{item}</li>) }
-                </ul>
-            </div>
+
         </div>
-        <div className='text-gray-1 text-heading-6 bg-royal-blue py-1 flex'>
-            <>© {new Date().getFullYear()} Phi Beta Sigma Bahamas. All rights reserved.</>
-            <div className='ml-auto'>Privacy</div>
+        <div className=' bg-royal-blue py-1'>
+            <div className='text-gray-1 text-heading-6 flex mx-auto max-w-screen-2xl px-10'>
+                <>© {new Date().getFullYear()} Phi Beta Sigma Bahamas. All rights reserved.</>
+                <div className='ml-auto'>Privacy</div>
+            </div>
+
         </div>
 
     </>
--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -10,15 +10,18 @@ export default function Navbar({type = 'main'}: Props) {
   return (
     <>
       <SubNavBar />
-      <div className='flex bg-royal-blue w-100 h-12 items-center'>
-        <PageLogo />
-        <div className='flex items-center ml-auto'>
-          {
-            renderNavigationLinks(mainNav[type])
-              .map((link, key) => <div key={key} className={`px-3 text-gray-1 hover:text-gray-3`}>{link}</div>)
-          }
-          <button className='text-royal-blue bg-gray-1 px-8 py-2 rounded text-heading-6 uppercase hover:text-dark-royal-blue hover:bg-gray-2'>Contact Us</button>
+      <div className='bg-royal-blue h-12 '>
+        <div className='flex max-w-screen-2xl items-center px-10 mx-auto'>
+          <PageLogo />
+          <div className='flex items-center ml-auto'>
+            {
+              renderNavigationLinks(mainNav[type])
+                .map((link, key) => <div key={key} className={`px-3 text-gray-1 hover:text-gray-3`}>{link}</div>)
+            }
+            <button className='text-royal-blue bg-gray-1 px-8 py-2 rounded text-heading-6 uppercase hover:text-dark-royal-blue hover:bg-gray-2'>Contact Us</button>
+          </div>
         </div>
+
       </div>
     </>
   )
@@ -26,11 +29,14 @@ export default function Navbar({type = 'main'}: Props) {
 
 function SubNavBar() {
   return (
-    <div className='flex bg-dark-royal-blue text-gray-1 text-sm'>
+    <div className=' bg-dark-royal-blue '>
+      <div className='max-w-screen-2xl px-10 flex text-gray-1 text-sm mx-auto'>
       {
         renderNavigationLinks(subNav)
           .map((link, key)=> <div key={key} className={`px-2 text-gray-1 hover:text-gray-3`}>{link}</div>)
       }
+      </div>
+
     </div>
   )
 }
