27ef53563ce0c6e2f67e69e4011a719d1e4d2b7f
Stanico <ashadknowles@gmail.com>
2023-02-20 22:00:03 -0500

    add get page by abbreviation method

---
+++ src/api/modules/chapterPages/chapterPages.ts
@@ -0,0 +1,16 @@
+import axiosRequest, { QueryParams } from "@/api/axios";
+
+const chapterPageActions = {
+  async getChapterPageByAbbreviation(chapterAbbreviation: string) {
+    const endpoint = "/chapter-pages";
+    const params: QueryParams = {
+      populate: "*",
+      "filters[chapter][chapter_abbreviation][$eq]": chapterAbbreviation,
+    };
+    return await axiosRequest(params)
+      .get(endpoint)
+      .catch((err: any) => null);
+  },
+};
+
+export default chapterPageActions;
