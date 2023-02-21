d307dd4b9e3ad78c8d8a072322af47c50078eb42
quincyarthur <quincyarthur@gmail.com>
2023-02-20 23:52:54 -0500

    add formatting to code

--- src/pages/chapter/[chapter_abbreviation]/line/[id].tsx
+++ src/pages/chapter/[chapter_abbreviation]/line/[id].tsx
@@ -25,7 +25,6 @@ export default function LineMembers({
   }
 
   const { chapter_abbreviation, id } = query;
-
   const [_lineMembers, setLineMembers] = useState(lineMembers);
   const [hasMore, setHasMore] = useState(true);
 
