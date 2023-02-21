d237a1d842e263e483d6653e95e78a5a61253f00
quincyarthur <quincyarthur@gmail.com>
2023-02-21 00:30:24 -0500

    move logic to get chapter lineage to separate function

--- src/pages/chapter/[chapter_abbreviation]/lineage/index.tsx
+++ src/pages/chapter/[chapter_abbreviation]/lineage/index.tsx
@@ -48,44 +48,56 @@ export default function Lineage({
     </div>
   );
 }
-
-export const getServerSideProps: GetServerSideProps<{
-  chapter_abbreviation: string;
-  chapter_name: string;
-  lineage: LineProps[];
-}> = async ({ query }) => {
-  let { chapter_abbreviation } = query;
-  chapter_abbreviation = (chapter_abbreviation as string).toUpperCase();
+async function getChapterLineage(
+  chapterAbbreviation: string
+): Promise<[string | undefined, LineProps[]]> {
+  chapterAbbreviation = chapterAbbreviation.toUpperCase();
   const token =
     "4300669fbc51d81c6ba5e2b2972dbb407e5512aecc3a8b3479a0936f75a3c9c4af610316dbcc131d0f2d30d7cb2a3c8bdd7f1c607256818c30a179f35771212ff40a172e614ccf6d3f8d0371eccf63997067c3b217566a8920875600d43d019b851c9243bc15c049e790670c25105e9bf39a64bfccef27edd065f80bb1258eba";
 
-  const url = `http://localhost:1337/api/chapters?populate[0]=lines&filters[chapter_abbreviation][$eq]=${chapter_abbreviation}`;
+  const url = `http://localhost:1337/api/chapters?populate[0]=lines&filters[chapter_abbreviation][$eq]=${chapterAbbreviation}`;
 
   const response = await fetch(url, {
     headers: { Authorization: `Bearer ${token}` },
   });
 
-  const json_data = await response.json();
+  const jsonChapterLineage = await response.json();
 
-  if (!json_data?.data?.length) {
-    return {
-      notFound: true,
-    };
+  if (!jsonChapterLineage?.data.length) {
+    return [undefined, []];
   }
+  const chapter_name = jsonChapterLineage.data[0].attributes?.name;
 
-  const chapter_name = json_data?.data[0].attributes?.name;
-  const lineage: LineProps[] = json_data?.data[0].attributes?.lines?.data.map(
-    (line: any) => {
+  const lineage: LineProps[] =
+    jsonChapterLineage?.data[0].attributes?.lines?.data.map((line: any) => {
       return {
         key: line.id,
         term: line.attributes?.term,
         year: line.attributes?.year,
         ship_name: line.attributes?.ship_name,
       };
-    }
-  );
+    });
+
+  return [chapter_name, lineage];
+}
+
+export const getServerSideProps: GetServerSideProps<{
+  chapter_abbreviation: string;
+  chapterName: string;
+  lineage: LineProps[];
+}> = async ({ query }) => {
+  let { chapter_abbreviation } = query;
+  chapter_abbreviation = (chapter_abbreviation as string).toUpperCase();
+
+  const [chapterName, lineage] = await getChapterLineage(chapter_abbreviation);
+
+  if (!chapterName) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
-    props: { chapter_abbreviation, chapter_name, lineage },
+    props: { chapter_abbreviation, chapterName, lineage },
   };
 };
