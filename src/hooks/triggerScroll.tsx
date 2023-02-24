dc221cc501620db365259d38a0279ccf1d356f78
quincyarthur <quincyarthur@gmail.com>
2023-02-24 02:48:15 -0500

    remove janky hook

--- src/hooks/triggerScroll.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-export function useTriggerScrollFix(deps) {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      window.dispatchEvent(new CustomEvent("scroll"));
-    }
-  }, deps);
-}
