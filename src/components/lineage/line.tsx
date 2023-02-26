a5734eb00e6050c7ead0bc05093859d005938628
quincyarthur <quincyarthur@gmail.com>
2023-02-26 11:37:43 -0500

    adjust text heading for term and year and adjust padding fot line component

--- src/components/lineage/line.tsx
+++ src/components/lineage/line.tsx
@@ -8,8 +8,8 @@ export interface LineProps {
 
 export default function Line(props: LineProps) {
   return (
-    <div className="rounded-lg shadow-lg p-8 bg-gray-1 hover:bg-gray-2 h-75px">
-      <span className="text-heading-4">
+    <div className="rounded-lg shadow-lg p-4 bg-gray-1 hover:bg-gray-2 max-h-[95px]">
+      <span className="text-heading-5">
         {props.term} {props.year}
       </span>
       <div className="text-heading-6">{props.shipName}</div>
