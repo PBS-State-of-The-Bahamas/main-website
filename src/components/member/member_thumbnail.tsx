014fe6e13788cb76685305210cf8cd334f1a37de
quincyarthur <quincyarthur@gmail.com>
2023-02-26 11:41:06 -0500

    adjust width and height of member thumbnail

--- src/components/member/member_thumbnail.tsx
+++ src/components/member/member_thumbnail.tsx
@@ -5,11 +5,7 @@ export interface MemberThumbnailProps {
 export default function MemberThumbnail(props: MemberThumbnailProps) {
   return (
     <div className="border">
-      <img
-        className="w-12 h-auto"
-        src={props.memberPhotoUrl}
-        alt="member"
-      ></img>
+      <img className="w-12 h-12" src={props.memberPhotoUrl} alt="member"></img>
     </div>
   );
 }
