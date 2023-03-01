4aec9ba94e72e7b662b14142afa38f69e54a29d4
Tanielle Arthur <taniellecurtis@Tanielles-MacBook-Pro.local>
2023-02-28 22:42:36 -0500

    handle situation where life member does not have their line information entered

--- src/components/lineage/line_member.tsx
+++ src/components/lineage/line_member.tsx
@@ -5,6 +5,9 @@ export interface LineMemberProps {
 }
 
 export default function LineMember(props: LineMemberProps) {
+  if (!(props.lineNumber && props.lineName)){
+    return <div></div>
+  }
   return (
     <div className="text-heading-6">
       #{props.lineNumber}-{props.lineName}
