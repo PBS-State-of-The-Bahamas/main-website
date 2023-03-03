c75006c9aa2978fc14869ec6fdf9f219c5643abe
Stanico <ashadknowles@gmail.com>
2023-03-03 17:10:02 -0500

    add get charter members function

--- src/api/modules/chapterPages/chapterPages.ts
+++ src/api/modules/chapterPages/chapterPages.ts
@@ -2,14 +2,30 @@ import axiosRequest, { QueryParams } from "@/api/axios";
 
 const chapterPageActions = {
   async getChapterPageByAbbreviation(chapterAbbreviation: string) {
-    const endpoint = "/chapter-pages";
-    const params: QueryParams = {
-      populate: "*",
-      "filters[chapter][chapter_abbreviation][$eq]": chapterAbbreviation?.toUpperCase(),
-    };
-    return await axiosRequest(params)
-      .get(endpoint)
-      .catch((err: any) => null);
+    try {
+      const endpoint = "/api/chapter-pages";
+      const params: QueryParams = {
+        populate: "*",
+        "filters[chapter][chapter_abbreviation][$eq]": chapterAbbreviation,
+      };
+      return await axiosRequest().get(endpoint, { params: params });
+    } catch (err: any) {
+      console.log(err);
+    }
+  },
+
+  async getCharterMembers(chapterAbbreviation: string) {
+    try {
+      const endpoint = "/api/line-members";
+      const params: QueryParams = {
+        "populate": "*",
+        "filters[line][chapter][chapter_abbreviation][$eq]": chapterAbbreviation,
+        "filters[line][charter_line][$eq]": "true",
+      };
+      return await axiosRequest().get(endpoint, { params: params });
+    } catch (err: any) {
+      console.log(err);
+    }
   },
 };
 
