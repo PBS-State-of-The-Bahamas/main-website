522d57c7c884376be0cd4d595fc77527723d87cd
Stanico Knowles <65465649+Stanico-Knowles@users.noreply.github.com>
2023-03-10 19:03:48 +0000

    add margin top and normal font weight

--- src/components/formElements/RadioGroup.tsx
+++ src/components/formElements/RadioGroup.tsx
@@ -4,19 +4,11 @@ const RadioGroup = ({ children, label, error}) => {
   return (
     <div>
       {label && (
-        <label className="block text-sm font-bold text-gray-6">{label}</label>
+        <label className="mt-1 block text-base font-normal text-gray-6">{label}</label>
       )}
       <fieldset className="mt-2">
         <div className="space-y-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-10">
           <div className="flex items-center">
-            {/* <input 
-            type="radio" 
-            id="customid" 
-            className="block text-sm font-medium leading-6 text-gray-6" 
-            onChange={formik.handleChange} 
-            name="enrolled" 
-            value="yes" 
-          /> adding comment for to just copy and paste later :) */}
             {children}
           </div>
         </div>
