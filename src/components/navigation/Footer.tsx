bde32334e7dd345631cc9aafb7e882fd815c145e
Stefano Hepburn <stefanohepburn@gmail.com>
2023-03-09 22:31:37 -0500

    modified footer component to use container

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { mainNav, renderNavigationLinks, subNav } from './data/pages';
+import Container from '../Container';
 
 type Props = {}
 
@@ -7,7 +8,9 @@ function Footer({}: Props) {
   return (
     <>
         <div className='bg-dark-royal-blue py-6'>
-            <div className='mx-auto flex space-x-20 max-w-screen-2xl px-10'>
+            <Container>
+            <div className='flex space-x-20'>
+                
                 <div>
                     <h6 className='text-heading-6 uppercase text-gray-1'>Menu</h6>
                     <ul className='text-gray-1 mt-3'>
@@ -20,14 +23,18 @@ function Footer({}: Props) {
                         {renderNavigationLinks(subNav).map((item, index)=> <li className='hover:text-gray-3'key={index}>{item}</li>) }
                     </ul>
                 </div>
+                
             </div>
+            </Container>
 
         </div>
-        <div className=' bg-royal-blue py-1 py-2'>
-            <div className='text-gray-1 text-heading-6 flex mx-auto max-w-screen-2xl px-10'>
+        <div className='bg-royal-blue py-2 text-gray-1 text-heading-6'>
+            <Container>
+                <div className='flex-col sm:flex-row flex w-full'>
                 <>© {new Date().getFullYear()} Phi Beta Sigma Bahamas. All rights reserved.</>
-                <div className='ml-auto'>Privacy</div>
-            </div>
+                <div className='sm:ml-auto mt-2'>Privacy</div>
+                </div>
+            </Container>
 
         </div>
 
