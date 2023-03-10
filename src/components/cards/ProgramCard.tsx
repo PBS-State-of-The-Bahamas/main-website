d8b5f451616710565037b0c0005b090b6a96fbf3
Stefano Hepburn <stefanohepburn@gmail.com>
2023-03-09 22:32:43 -0500

    added Program card component

--- src/components/cards/ProgramCard.tsx
+++ src/components/cards/ProgramCard.tsx
@@ -7,8 +7,8 @@ type Props = {
 
 export default function ProgramCard({title, photoUrl}: Props) {
   return (
-    <div className="rounded w-1/4 h-72 border-gray-2 border bg-gray-1">
-        <div className="bg-gray-3 w-full h-5/6 rounded-t">{photoUrl}</div>
+    <div className="rounded my-2 lg:my-0 lg:w-1/4 grow h-72 border-gray-2 border bg-gray-1">
+        <div className={`${!photoUrl && 'bg-[#F0F0F0]'} w-full h-5/6 rounded-t`}><img src={photoUrl || '/images/empty.png'} alt={title} className='h-full mx-auto'/></div>
         <h5 className="text-heading-5 p-2">{title} </h5>
     </div>
   )
