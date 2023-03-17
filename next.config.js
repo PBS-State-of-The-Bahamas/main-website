da27f7d45c901989486d43e45b914cb41b3d24ca
quincyarthur <quincyarthur@gmail.com>
2023-03-17 10:40:39 -0400

    remove env vars from next config

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
   images: {
     domains: ["localhost", "admin.sigmabahamas1914.org"],
   },
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    NEXT_PUBLIC_TOKEN: process.env.NEXT_PUBLIC_TOKEN,
-  },
 };
 
 module.exports = nextConfig;
