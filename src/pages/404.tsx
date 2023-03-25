23445d8a5b7c0642ece7b129c34e9e4494ac331b
Stefano Hepburn <stefanohepburn@gmail.com>
2023-03-24 20:53:20 -0400

    Remove dove from 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -9,7 +9,7 @@ export default function FourOhFour() {
     <div className="min-h-[75%] bg-gray-0">
         <Section>
           <div className='flex flex-col text-center w-full relative'>
-            <img src="/images/404-dove.png" alt="Dove" className='w-40 mt-8 mx-auto'/>
+            <h2 className='text-gray-6 text-[80px] leading-[80px] font-bold'>404</h2>
             <h3 className="text-heading-4">You might be lost</h3>
             <p className='text-gray-5'>This page does not exist or was removed. Let&apos;s go back home.</p>
             <Link href='/'><button className="cursor-pointer text-heading-6 bg-royal-blue text-gray-1 py-3 px-6 rounded mt-6 uppercase">Back to homepage</button></Link>
