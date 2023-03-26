1877b638359b7bb0d596846638b8c18ea2668d7f
Stanico-Knowles <ashadknowles@gmail.com>
2023-03-26 17:47:23 -0400

    handle form errors and submission

--- src/components/forms/ChapterInterestForm.tsx
+++ src/components/forms/ChapterInterestForm.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import InputField from "../formElements/InputField";
 import * as Yup from "yup";
 import { useFormik } from "formik";
 import RadioGroup from "../formElements/RadioGroup";
 import FormButton from "../formElements/FormButton";
 import chapterPageActions from "@/api/modules/chapterPages/chapterPages";
+import sendToast from "@/util/toast/toast";
+import { ToastTypes } from "@/util/toast/enums/toastTypes";
 
 enum FormFeedbackEnum {
   FIELD_REQUIRED = "This is required.",
@@ -30,6 +32,7 @@ export interface EmailData {
 }
 
 const ChapterInterestForm = (props: Props) => {
+  const [errors, setErrors] = useState<string[] | null>(null);
   const formik = useFormik({
     initialValues: {
       fullName: "",
@@ -66,7 +69,20 @@ const ChapterInterestForm = (props: Props) => {
           },
         },
       };
-      await chapterPageActions.sendChapterInterestEmail(emailData)
+      const [res, errors] = await chapterPageActions.sendChapterInterestEmail(
+        emailData
+      );
+      if (errors) {
+        setErrors(errors);
+        return;
+      }
+      if (res.success) {
+        setErrors(null);
+        formik.setValues(formik.initialValues);
+        formik.setTouched(formik.initialTouched);
+        window.location.reload();
+        sendToast("Form submitted successfully!", ToastTypes.SUCCESS);
+      }
     },
     validationSchema:
       props.chapterType === "undergraduate"
@@ -105,6 +121,28 @@ const ChapterInterestForm = (props: Props) => {
     <div className="max-w-screen-sm flex flex-col mx-auto mt-8 mb-16 max-sm:px-2">
       <h4 className="text-heading-4 py-4">Interested in Joining</h4>
       <form className="flex flex-col" onSubmit={(e: any) => submit(e)}>
+        {errors && (
+          <div className="rounded-md bg-red-50 p-4">
+            <div className="flex">
+              <div className="ml-3">
+                <h3 className="text-sm font-medium text-feedback-warning">
+                  {errors.length > 1 ? (
+                    <>There were {errors.length} errors with your submission</>
+                  ) : (
+                    <>There was {errors.length} error with your submission</>
+                  )}
+                </h3>
+                <div className="mt-2 text-sm text-feedback-warning">
+                  <ul role="list" className="list-disc space-y-1 pl-5">
+                    {errors.map((error: string) => (
+                      <li>{error}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
         <InputField
           label="Full name"
           name="fullName"
