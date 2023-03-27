6b566023f7adf493e52f4a58b8e7e8391ceb0313
Stanico-Knowles <ashadknowles@gmail.com>
2023-03-26 21:12:04 -0400

    add optional onclick property

--- src/components/formElements/FormButton.tsx
+++ src/components/formElements/FormButton.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 type Props = {
   text: string;
+  onClick?: () => void;
 };
 
-const FormButton = ({ text }: Props) => {
+const FormButton = ({ text, onClick }: Props) => {
   return (
     <button
       type="submit"
+      onClick={() => onClick && onClick()}
       className="mt-3 flex w-full uppercase justify-center bg-royal-blue py-2 px-4 text-sm font-bold text-gray-1 shadow-sm hover:bg-dark-royal-blue disabled:bg-blue-200"
     >
       {text}
