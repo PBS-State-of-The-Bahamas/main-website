4296659627119f80ab37d6b2caa4050c96781c9c
Stanico-Knowles <ashadknowles@gmail.com>
2023-03-26 21:03:41 -0400

    remove relative position property

--- src/components/formElements/InputField.tsx
+++ src/components/formElements/InputField.tsx
@@ -30,7 +30,7 @@ const InputField = ({
       <label htmlFor={name} className="block text-base font-normal text-gray-6">
         {label}
       </label>
-      <div className="relative mt-1 rounded-md">
+      <div className="mt-1 rounded-md">
         <input
           id={name}
           name={name}
