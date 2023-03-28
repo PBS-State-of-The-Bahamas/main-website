99a28e409058642cacc7173f9660fb9460f00d3b
Quincy Arthur <quincyarthur@Quincys-MacBook-Pro.local>
2023-03-28 08:36:48 -0400

    idk how this was working before but correcting component name

--- src/components/e-board/BoardMemberThumbnail.tsx
+++ src/components/e-board/BoardMemberThumbnail.tsx
@@ -2,7 +2,7 @@ export interface BoardMemberThumbnailProps {
     boardMemberPhotoUrl: string;
   }
   
-  export default function MemberThumbnail(props: BoardMemberThumbnailProps) {
+  export default function BoardMemberThumbnail(props: BoardMemberThumbnailProps) {
     return (
       <div className="border">
         <img className="w-[324px] h-[223px]" src={props.boardMemberPhotoUrl} alt="member"></img>
