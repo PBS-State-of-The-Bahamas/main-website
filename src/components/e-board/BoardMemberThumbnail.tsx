71fcb82232c3e61d8c01dc1858015bede4d59d6c
Quincy Arthur <quincyarthur@Quincys-MacBook-Pro.local>
2023-03-28 08:20:41 -0400

    use the exact height and width in the figma spec

--- src/components/e-board/BoardMemberThumbnail.tsx
+++ src/components/e-board/BoardMemberThumbnail.tsx
@@ -5,7 +5,7 @@ export interface BoardMemberThumbnailProps {
   export default function MemberThumbnail(props: BoardMemberThumbnailProps) {
     return (
       <div className="border">
-        <img className="w-20 h-13" src={props.boardMemberPhotoUrl} alt="member"></img>
+        <img className="w-[324px] h-[223px]" src={props.boardMemberPhotoUrl} alt="member"></img>
       </div>
     );
   }
