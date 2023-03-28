3a843be34652a30f7d38a9bcef7fd3ed5edd9acc
Quincy Arthur <quincyarthur@Quincys-MacBook-Pro.local>
2023-03-27 23:37:43 -0400

    style e board

--- src/components/e-board/BoardMember.tsx
+++ src/components/e-board/BoardMember.tsx
@@ -4,16 +4,15 @@ export interface BoardMemberProps {
   memberName: string;
   memberPhotoUrl: string;
   position: string
-
 }
 
 export default function BoardMember(props: BoardMemberProps) {
   return (
-    <div className="p-2 h-[75px]">
+    <div className="p-2 h-13">
       <BoardMemberThumbnail boardMemberPhotoUrl={props.memberPhotoUrl} />
       <div className="">
         <div className="text-heading-5">{props.memberName}</div>
-        <div className="text-heading-5">{props.position}</div>
+        <div className="">{props.position}</div>
       </div>
     </div>
   );
