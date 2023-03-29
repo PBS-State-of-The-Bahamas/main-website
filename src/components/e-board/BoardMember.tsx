6fa2401b4c0ec79ca5d62d2f5ae97d750aa92bc0
quincyarthur <quincyarthur@gmail.com>
2023-03-29 18:13:20 -0400

    remove unnecessary padding

--- src/components/e-board/BoardMember.tsx
+++ src/components/e-board/BoardMember.tsx
@@ -8,7 +8,7 @@ export interface BoardMemberProps {
 
 export default function BoardMember(props: BoardMemberProps) {
   return (
-    <div className="p-2 h-13">
+    <div className="h-13">
       <BoardMemberThumbnail boardMemberPhotoUrl={props.memberPhotoUrl} />
       <div className="text-heading-5">{props.memberName}</div>
       <div>{props.position}</div>
