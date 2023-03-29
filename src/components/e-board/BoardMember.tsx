703a2771aac30d4f3cc83822d55ad1dc4b1b6558
quincyarthur <quincyarthur@gmail.com>
2023-03-29 18:07:21 -0400

    remvove unnecessary div

--- src/components/e-board/BoardMember.tsx
+++ src/components/e-board/BoardMember.tsx
@@ -3,17 +3,15 @@ import BoardMemberThumbnail from "../e-board/BoardMemberThumbnail";
 export interface BoardMemberProps {
   memberName: string;
   memberPhotoUrl: string;
-  position: string
+  position: string;
 }
 
 export default function BoardMember(props: BoardMemberProps) {
   return (
     <div className="p-2 h-13">
       <BoardMemberThumbnail boardMemberPhotoUrl={props.memberPhotoUrl} />
-      <div className="">
-        <div className="text-heading-5">{props.memberName}</div>
-        <div className="">{props.position}</div>
-      </div>
+      <div className="text-heading-5">{props.memberName}</div>
+      <div>{props.position}</div>
     </div>
   );
 }
