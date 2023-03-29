9303f1913fb01a3a19502759ad8265291c1369c9
quincyarthur <quincyarthur@gmail.com>
2023-03-29 11:46:45 -0400

    use next image component vs img tag

--- src/components/e-board/BoardMemberThumbnail.tsx
+++ src/components/e-board/BoardMemberThumbnail.tsx
@@ -1,12 +1,19 @@
+import Image from "next/image";
+
 export interface BoardMemberThumbnailProps {
-    boardMemberPhotoUrl: string;
-  }
-  
-  export default function BoardMemberThumbnail(props: BoardMemberThumbnailProps) {
-    return (
-      <div className="border">
-        <img className="w-[324px] h-[223px]" src={props.boardMemberPhotoUrl} alt="member"></img>
-      </div>
-    );
-  }
-  
+  boardMemberPhotoUrl: string;
+}
+
+export default function BoardMemberThumbnail(props: BoardMemberThumbnailProps) {
+  return (
+    <div className="border">
+      <Image
+        src={props.boardMemberPhotoUrl}
+        alt={"member"}
+        width={324}
+        height={223}
+        className={"w-[324px] h-[223px]"}
+      ></Image>
+    </div>
+  );
+}
