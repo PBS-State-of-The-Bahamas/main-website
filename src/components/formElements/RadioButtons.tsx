8c0250b734238e61eec86af243e4b28c01cbc520
Stefano Hepburn <stefanohepburn@gmail.com>
2023-03-30 11:10:29 -0400

    Added flow direction to RadioButtons to allow for column and rows

--- src/components/formElements/RadioButtons.tsx
+++ src/components/formElements/RadioButtons.tsx
@@ -5,18 +5,23 @@ type Props = {
     options: {key: string, value: string | number}[];
     for: string;
     onChange: any;
+    flowDirection?: string;
 }
 
 const RadioButtons = (props: Props) => {
+    const direction = {
+        row: 'inline first:ml-0 ml-2',
+        column: 'block'
+    }
   return (
     <div>
         <h5>{props.label}</h5>
         <fieldset>
             {
                 props.options.map((option, index) => (
-                    <div key={option.key}>
+                    <div key={option.key} className={`${direction[props.flowDirection? props.flowDirection : direction['column']]}`}>
                         <input type="radio" name={props.for} id={option.key} value={option.value} onChange={props.onChange} defaultChecked={index == 0 && true}/>
-                        <label htmlFor={option.key} className='uppercase ml-2 text-heading-6'>{option.key}</label>
+                        <label htmlFor={option.key} className={`uppercase ml-2 text-heading-6`}>{option.key}</label>
                     </div>
 
                 ))
