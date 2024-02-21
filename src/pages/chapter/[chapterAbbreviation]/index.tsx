56f08b239b6e195407d9f7e970d3c528a2a8ee62
Stanico Knowles <ashadknowles@gmail.com>
2024-02-20 21:14:17 -0500

    fixed rendering of rich text fields

--- src/pages/chapter/[chapterAbbreviation]/index.tsx
+++ src/pages/chapter/[chapterAbbreviation]/index.tsx
@@ -11,6 +11,7 @@ import React from "react";
 import ChapterInterestForm from "@/components/forms/ChapterInterestForm";
 import Head from "next/head";
 import Image from "next/image";
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 export interface Chapter {
   name: string;
@@ -72,17 +73,35 @@ const Index: NextPage = ({ ...data }: PageData) => {
   });
   const images = data.images.map((image: ChapterImage) => {
     return {
-      source: `${process.env.NEXT_PUBLIC_API_PROTOCOL}://${process.env.NEXT_PUBLIC_API_HOST}:${process.env.NEXT_PUBLIC_API_PORT}${image.source}`,
+      source: `${process.env.NEXT_PUBLIC_API_PROTOCOL}://${process.env.NEXT_PUBLIC_API_HOST}${process.env.NEXT_PUBLIC_API_PORT}${image.source}`,
       altText: image.altText,
     };
   });
-  const imageIndex = Math.floor(Math.random() * images.length)
+  const imageIndex = Math.floor(Math.random() * images.length);
   const socials = data.chapterSocials.map((social: ChapterSocial) => {
     return {
       platform: social.platform,
       profileURL: social.profileURL,
     };
   });
+  const historyParagraphs = data.history.split("\n");
+
+  // Replace occurrences of **Text** with the corresponding bolded React component
+  const formattedParagraphs = historyParagraphs.map(
+    (paragraph, paragraphIndex) => {
+      const formattedText = paragraph
+        .split(/\*\*(.*?)\*\*/g)
+        .map((segment, index) => {
+          return index % 2 === 0 ? (
+            segment
+          ) : (
+            <strong key={index}>{segment}</strong>
+          );
+        });
+
+      return <p key={paragraphIndex}>{formattedText}</p>;
+    }
+  );
 
   return (
     <>
@@ -98,43 +117,53 @@ const Index: NextPage = ({ ...data }: PageData) => {
           imageURL={images[Math.floor(Math.random() * images.length)].source}
           socials={socials}
         />
-        <div className="flex flex-col items-center lg:flex-row w-full">
-          <div className="lg:w-3/6 p-6">
-            <h4 className="text-heading-4 pb-8 font-bold text-gray-6">
-              History
-            </h4>
-            <Image
-              src={images[imageIndex].source}
-              alt={images[imageIndex].altText}
-              width={400}
-              height={300}
-              className="object-fit w-[400px] h-[300px]"
-            />
-          </div>
-          <div className="lg:w-3/6 p-6">
-            <p className="text-gray-5">{data.history}</p>
-          </div>
-        </div>
-        <div className="m-8">
-          <h4 className="text-heading-4 font-bold text-gray-6">Charters</h4>
-        </div>
-        <div className="m-8 grid gap-5 max-sm:grid-cols-1 max-sm:gap-6 md:grid-cols-3 lg:grid-cols-4">
-          {charters.map((charter, idx) => (
-            <Member
-              key={charter.title}
-              memberName={charter.title}
-              memberPhotoUrl={charter.imageURL}
-            >
-              <LineMember
-                key={charter.title}
-                id={idx}
-                lineNumber={parseInt(charter.subtitle)}
-                lineName={charter.description}
+        <div className="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8">
+          <div className="mx-auto flex flex-col items-start lg:mx-0 lg:max-w-none lg:flex-row">
+            <div className="w-full lg:flex-auto">
+              <h4 className="text-heading-4 tracking-wider pb-8 font-bold text-gray-6">
+                Chapter History
+              </h4>
+              <Image
+                src={images[imageIndex].source}
+                alt={images[imageIndex].altText}
+                width={400}
+                height={300}
+                className="object-fit w-[400px] h-[300px]"
               />
-            </Member>
-          ))}
+            </div>
+            <div className="w-full text-sm lg:text-base lg:flex-auto mt-8 lg:mt-0 space-y-2">
+              {formattedParagraphs}
+            </div>
+          </div>
         </div>
-        <CardsGrid heading="Awards" items={awards} />
+        <div>
+          {charters?.length > 0 && (
+            <>
+              <div className="m-8">
+                <h4 className="text-heading-4 font-bold text-gray-6">
+                  Charters
+                </h4>
+              </div>
+              <div className="m-8 grid gap-5 max-sm:grid-cols-1 max-sm:gap-6 md:grid-cols-3 lg:grid-cols-4">
+                {charters.map((charter, idx) => (
+                  <Member
+                    key={charter.title}
+                    memberName={charter.title}
+                    memberPhotoUrl={charter.imageURL}
+                  >
+                    <LineMember
+                      key={charter.title}
+                      id={idx}
+                      lineNumber={parseInt(charter.subtitle)}
+                      lineName={charter.description}
+                    />
+                  </Member>
+                ))}
+              </div>
+            </>
+          )}
+        </div>
+        {awards?.length > 0 && <CardsGrid heading="Awards" items={awards} />}
         <GridGallery images={images} />
         <ChapterInterestForm
           chapter={data.chapter.chapterAbbreviation?.toUpperCase()}
@@ -212,7 +241,7 @@ const fromApiResponseToChapterCharterInterface = (
   lineNumber: charter?.attributes?.line_number,
   imageURL: charter?.attributes?.member?.data?.attributes?.photo?.data?.[0]
     ?.attributes?.formats?.small?.url
-    ? `${process.env.NEXT_PUBLIC_API_PROTOCOL}://${process.env.NEXT_PUBLIC_API_HOST}:${process.env.NEXT_PUBLIC_API_PORT}${charter?.attributes?.member?.data?.attributes?.photo?.data[0]?.attributes?.formats?.small?.url}`
+    ? `${process.env.NEXT_PUBLIC_API_PROTOCOL}://${process.env.NEXT_PUBLIC_API_HOST}${process.env.NEXT_PUBLIC_API_PORT}${charter?.attributes?.member?.data?.attributes?.photo?.data[0]?.attributes?.formats?.small?.url}`
     : "/images/missing-member.svg",
 });
 
--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -15,7 +15,24 @@ export default function History( {
   if(!history) {
     return notFound
   }
+  const historyParagraphs = history.split("\n");
 
+  // Replace occurrences of **Text** with the corresponding bolded React component
+  const formattedParagraphs = historyParagraphs.map(
+    (paragraph, paragraphIndex) => {
+      const formattedText = paragraph
+        .split(/\*\*(.*?)\*\*/g)
+        .map((segment, index) => {
+          return index % 2 === 0 ? (
+            segment
+          ) : (
+            <strong key={index}>{segment}</strong>
+          );
+        });
+
+      return <p key={paragraphIndex}>{formattedText}</p>;
+    }
+  );
   return (
     <div>
       <Head>
@@ -25,9 +42,9 @@ export default function History( {
         <Section>
           <div>
               <h3 className="text-heading-3">History of state</h3>
-                <p className=" pt-2 text-gray-5">
-                  {history}
-                </p>
+                <div className="space-y-4 pt-2 text-gray-5">
+                  {formattedParagraphs}
+                </div>
           </div>
         </Section>
       </PageTemplate>
